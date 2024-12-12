f3ff1846406a28438ed1e0662334e875c027e94e
astorelloo astorelloo <lil.astor06@gmail.com>
2024-12-12 08:47:49 +0000

    ci stiamo lavorando

--- progetto/moduls.py
+++ progetto/moduls.py
@@ -4,3 +4,7 @@ class lista_carte(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False) 
     descrizione = db.Column(db.String(100), nullable = False)
+    immagine = db.Column(db)
+class ListaUtenti(db.Model):
+    nome = db.Column(db.String(100), primary_key = True)
+    password = db.Column(db.String(100), primary_key = True)
